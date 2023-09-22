8c2825e29931760c7721e868bf90ebfa9daf1b6d
Jaebin <jaebin@sosohan.team>
2023-09-22 13:45:26 +0900

    Initial Commit

---
+++ AnimatedCarousel/AnimatedCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedCarouselApp.swift
+//  AnimatedCarousel
+//
+//  Created by JaeBin on 2023/09/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
